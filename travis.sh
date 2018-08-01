a55f6983faebc60fbe4ea238d4c67de6526121ad
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-08-01 13:42:32 +0200

    fixed test file

--- travis.sh
+++ travis.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ev
-cd passwd
 if [ "${COVERAGE}" = "true" ]; then
   go test -v -covermode=count -coverprofile=coverage.out ./...
   goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
