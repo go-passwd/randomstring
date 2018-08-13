c52d046b806225dc8d1284c886209cc8b513f5d3
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-08-13 07:21:07 +0200

    added symbols and documentation

--- chars.go
+++ chars.go
@@ -2,7 +2,15 @@ package randomstring
 
 // Base characters allowed in a password
 const (
+	// LowerLetters is a sets of lowercase latin letters
 	LowerLetters = "abcdefghijklmnopqrstuvwxyz"
+
+	// UpperLetters is a set of uppercase latin letters
 	UpperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	Digits       = "1234567890"
+
+	// Digits is a set of digits
+	Digits = "1234567890"
+
+	// Symbols is a set of symbols
+	Symbols = "!\";#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 )
